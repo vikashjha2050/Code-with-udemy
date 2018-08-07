fe04941bf99a762950a715ed0b58f7164e7b6ee4
vikash kumar <vikashjha2050@gmail.com>
2018-08-07 19:44:39 +0530

    Add files via upload

---
+++ jsfolder/js_l1_pro.js
@@ -0,0 +1,10 @@
+var First = prompt("Please enter your name");
+var last= prompt("last name ");
+var age=prompt("age??");
+var height=prompt("height in cms??");
+var pet=prompt("pet name");
+if (First[0]==last[0]&&age>20&&age<30&&height>=170&&pet[pet.length-1]=="y")
+console.log("Hello comrade!");
+else {
+  console.log("nothing to do");
+}
