3dbe2b4af57b275de1a2f408af9138891f784323
vikash kumar <vikashjha2050@gmail.com>
2018-08-12 17:02:18 +0530

    Delete admin.py

--- usermodel1/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from usermodel1.models import userinfo
-# Register your models here.
-
-admin.site.register(userinfo)
