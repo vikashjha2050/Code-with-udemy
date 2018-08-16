6509ecc6b60a53c97d752a0e2db802bdf264b796
vikash kumar <vikashjha2050@gmail.com>
2018-08-17 01:20:13 +0530

    Delete inheriviews.py

--- templateinheri/inheriviews.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
-
-def inheri_fun(request):
-    return render(request,'templateinheri/inheri1.html')
